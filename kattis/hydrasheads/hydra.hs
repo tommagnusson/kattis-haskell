2070ab2c3d4af94523ca18ab3d86b506a63dbad8
Thomas Magnusson <thomas.magnusson1@marist.edu>
2019-02-08 01:34:27 -0500

    adds not much progress

--- kattis/hydrasheads/hydra.hs
+++ kattis/hydrasheads/hydra.hs
@@ -23,4 +23,10 @@ tuplify' [] = undefined
 tuplify' (x:xs) = (x, head xs)
 
 fight :: Hydra -> Integer
-fight (0, 1) = -1
+fight (0, 0) = 0
+fight (1, 0) = -1
+fight (2, 0) = 1
+fight (0, h) | h % 2 == 0 = h / 2
+             | otherwise  = h / 2 + 6
+fight (t, 0) | t % 4 == 0 = (t / 2) + (t / 4)
+             | t % 2 == 0 = (t / 2)
