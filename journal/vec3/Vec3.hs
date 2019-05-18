579d717d9644e2a1864dce796949242e84d56f99
Thomas Magnusson <thomas.magnusson1@marist.edu>
2019-05-18 14:10:36 -0400

    adds entry 17 and adds to Vec3

--- journal/vec3/Vec3.hs
+++ journal/vec3/Vec3.hs
@@ -5,7 +5,35 @@ magnitude :: Vec3 -> Double
 magnitude (Vec3 x y z) = sqrt (x^2 + y^2 + z^2)
 
 normalize :: Vec3 -> Vec3
-normalize v@(Vec3 x y z) = undefined
+normalize v@(Vec3 x y z) = Vec3 (x / m) (y / m) (z / m)
+  where m = magnitude v
+
+
+-- I know the scalar product can be found somehow using lists of triples as Monad...
+scalarProduct :: Vec3 -> Vec3 -> Double
+scalarProduct v@(Vec3 x y z) w@(Vec3 a b c) = 
+  x * a + x * b + x * c + y * a + y * b + y * c + z * a + z * b + z * c 
+
+dotProduct :: Vec3 -> Vec3 -> Double
+dotProduct = scalarProduct
+
+-- little bit of an asymmetric implementation between
+-- the Vec2 type alias and the new data type of Vec3
+-- But oh well
+type Vec2 = (Double, Double)
+
+mag :: Vec2 -> Double
+mag v@(Vec2 x y) = sqrt (x^2 + y^2)
+
+dotProd :: Vec2 -> Vec2 -> Double
+dotProd (x, y) (a, b) = x * a + y * b
+
+ang :: Vec2 -> Vec2 -> Double
+ang v@(x, y) w@(a, b) = dotProd v w / (mag v * mag w)
+
+-- TODO
+crossProduct :: Vec2 -> Vec2 -> Vec3
+crossProduct v@(x, y) w@(a, b) = mag v * mag w * (sin $ ang v w) * n
 
 instance Num Vec3 where
   (+) (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 + x2, y1 + y2, z1 + z2)
