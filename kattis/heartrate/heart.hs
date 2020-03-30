f163c61030f9b440378b49d2d3f3f5b942507c8e
Thomas Magnusson <thomas.magnusson1@marist.edu>
2020-03-29 22:28:36 -0400

    solves heartrate

--- kattis/heartrate/heart.hs
+++ kattis/heartrate/heart.hs
@@ -1,5 +1,5 @@
 parse :: String -> [[Double]]
-parse = (map ((map read) . words)). tail . lines
+parse = (map ((map read) . words)) . tail . lines
 
 solve :: [[Double]] -> [[Double]]
 solve = map solve'
@@ -7,8 +7,8 @@ solve = map solve'
 solve' :: [Double] -> [Double]
 solve' [b, p] = [bpm - d, bpm, bpm + d]
   where
-    d  = 60 / p
-    bpm = d * p 
+    bpm = (60 * b) / p
+    d = 60 / p
 
 display :: [[Double]] -> String
 display = unlines . map (unwords . (map show))
