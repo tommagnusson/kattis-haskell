c10b00c92abb3e77d3bc05514a4292c44e828517
Thomas Magnusson <thomas.magnusson1@marist.edu>
2020-03-31 22:04:42 -0400

    solves speedlimit

--- kattis/speedlimit/speedlimit.hs
+++ kattis/speedlimit/speedlimit.hs
@@ -1,15 +1,35 @@
-parse :: String -> [[(Int, Int)]]
-parse s = undefined
+parse :: String -> [[[Int]]]
+parse = cases (map read . words). init . lines
 
--- break after single element
+solve :: [[[Int]]] -> [Int]
+solve = map solve'
 
-solve :: a -> b
-solve = undefined
+solve' :: [[Int]] -> Int
+solve' = fst . foldl solve'' (0, 0)
 
-display :: a -> String
-display = undefined
+solve'' :: (Int, Int) -> [Int] -> (Int, Int)
+solve'' (m, h) [mph, hr] 
+    | (m, h) == (0,0) = (mph * hr, hr)
+    | otherwise = (m + mph * (hr - h), hr)
+
+display :: [Int] -> String
+display = unlines . map (\x -> show x ++ " miles")
 
 main :: IO ()
 main = interact (display . solve . parse)
 
 example = "3\n20 2\n30 6\n10 7\n2\n60 1\n30 5\n4\n15 1\n25 2\n30 3\n10 5\n-1"
+
+-- For a list of strings with test cases separated by a number
+cases :: (String -> a) -> [String] -> [[a]]
+cases f ss = cases' f ss []
+
+cases' :: (String -> a) -> [String] -> [[a]] -> [[a]]
+cases' f ss acc 
+    | length ss == 0 = acc
+    | otherwise = cases' f rest (acc ++ [g])
+    where
+        n = read $ head ss
+        g = map f $ take n $ tail ss
+        rest = drop n $ tail ss
+
--- templates/lib.hs
+++ templates/lib.hs
@@ -8,3 +8,17 @@ splitEvery _ [] = []
 splitEvery n list = first : (splitEvery n rest)
     where
         (first, rest) = splitAt n list
+
+-- For a list of strings, starting with a number n, 
+-- and n test cases, then another number m, m test cases, etc
+cases :: (String -> a) -> [String] -> [[a]]
+cases f ss = cases' f ss []
+
+cases' :: (String -> a) -> [String] -> [[a]] -> [[a]]
+cases' f ss acc 
+    | length ss == 0 = acc
+    | otherwise = cases' f rest (acc ++ [g])
+    where
+        n = read $ head ss
+        g = map f $ take n $ tail ss
+        rest = drop n $ tail ss
