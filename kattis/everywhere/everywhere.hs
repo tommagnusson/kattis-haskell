d27af22a78a75b032b4bc859e25fbb2aa3d7d832
Thomas Magnusson <thomas.magnusson1@marist.edu>
2020-03-31 22:42:32 -0400

    solves everywhere

--- kattis/everywhere/everywhere.hs
+++ kattis/everywhere/everywhere.hs
@@ -1,15 +1,28 @@
-import Data.Maybe
-import Text.Read
-import Data.List
+import Data.List (nub)
 
-parse :: String -> [Maybe Integer]
-parse = map (readMaybe :: Maybe Integer) . tail . lines
+parse :: String -> [[String]]
+parse = cases id . tail . lines
 
 solve :: [[String]] -> [Int]
-solve = map (length . nub)
-4
+solve = map solve'
+
+solve' :: [String] -> Int
+solve' = length . nub
+
 display :: [Int] -> String
 display = unlines . map show
 
 main :: IO ()
 main = interact (display . solve . parse)
+
+cases :: (String -> a) -> [String] -> [[a]]
+cases f ss = cases' f ss []
+
+cases' :: (String -> a) -> [String] -> [[a]] -> [[a]]
+cases' f ss acc 
+    | length ss == 0 = acc
+    | otherwise = cases' f rest (acc ++ [g])
+    where
+        n = read $ head ss
+        g = map f $ take n $ tail ss
+        rest = drop n $ tail ss
